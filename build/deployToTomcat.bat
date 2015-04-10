306a23fb7347264a2e15e78bdca4b0f87b336e92
Steve Schmitt <steves@ideablade.com>
2015-04-10 10:18:13 -0700

    Fixed path to breeze-webtest.war in deployToTomcat.bat

--- build/deployToTomcat.bat
+++ build/deployToTomcat.bat
@@ -1,7 +1,7 @@
 :: Copy the WAR to tomcat's webapps dir, and start tomcat
 call setenv.bat
 rmdir %CATALINA_HOME%\webapps\breeze-webtest /S /Q 
-copy breeze-webtest\target\breeze-webtest.war %CATALINA_HOME%\webapps
+copy ..\breeze-webtest\target\breeze-webtest.war %CATALINA_HOME%\webapps
 pushd %CATALINA_HOME%\bin
 call catalina jpda start
 popd
