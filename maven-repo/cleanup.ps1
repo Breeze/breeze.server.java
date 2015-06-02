e1d3a497a7f1aec6da89f8ac1b15a4b01e9ab2cb
Steve Schmitt <steves@ideablade.com>
2015-06-01 23:02:46 -0700

    Remove cleanup.ps1

--- maven-repo/cleanup.ps1
+++
@@ -1,5 +0,0 @@
-# change maven-metadata-local.xml to maven-metadata.xml
-Get-ChildItem -Recurse | Rename-Item -NewName {$_.name -replace "-local","" }
-
-# remove _remote.repositories
-Get-ChildItem -Filter "_remote.repositories" -Recurse | Remove-Item 
